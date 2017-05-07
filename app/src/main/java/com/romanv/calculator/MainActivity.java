a954d53a6abc63ac297f2d0aa695e3f0396c5de7
Vorobyov-Roman <vorobrawr@gmail.com>
2017-05-07 05:03:54 +0300

    Close #1 Basic user interface.

--- app/src/main/java/com/romanv/calculator/MainActivity.java
+++ app/src/main/java/com/romanv/calculator/MainActivity.java
@@ -1,13 +1,43 @@
 package com.romanv.calculator;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        setupActionBar(R.id.toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setupActionBar(@IdRes int toolbarId) {
+        Toolbar toolbar = (Toolbar) findViewById(toolbarId);
+        setSupportActionBar(toolbar);
     }
 }
